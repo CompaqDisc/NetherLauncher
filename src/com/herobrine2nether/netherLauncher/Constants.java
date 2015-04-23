79bcc35cf7554a7ef1f0ca2d9589fc1ad0e2bca2
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-04-23 09:31:36 -0600

    Adding IDEA Workspace

---
+++ src/com/herobrine2nether/netherLauncher/Constants.java
@@ -0,0 +1,8 @@
+package com.herobrine2nether.netherLauncher;
+
+/**
+ * Created by Owner on 4/22/2015.
+ */
+public class Constants {
+    public static final String VERSION = "0.1.0";
+}
---
+++ src/com/herobrine2nether/netherLauncher/Main.java
@@ -0,0 +1,40 @@
+package com.herobrine2nether.netherLauncher;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                gui();
+            }
+        });
+    }
+
+    public static void gui() {
+        JFrame window = new JFrame("NetherLauncher " + Constants.VERSION);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        URL iconUrl = null;
+        try {
+            iconUrl = new URL("https://raw.githubusercontent.com/Herobrine2Nether/NetherLauncher/master/Head16.png");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        java.awt.Image icon = java.awt.Toolkit.getDefaultToolkit().getDefaultToolkit().createImage(iconUrl);
+        window.setIconImage(icon);
+
+        JLabel label = new JLabel("NetherLauncher " + Constants.VERSION + " is starting up...");
+        window.getContentPane().add(label);
+
+        window.pack();
+        window.setVisible(true);
+    }
+}
