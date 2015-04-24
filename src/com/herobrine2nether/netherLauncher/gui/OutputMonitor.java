95ec13dbb318ce3c51e6313dcb200d0277a14f60
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-04-24 11:51:36 -0600

    IDEA Workspace Corrupted

--- src/com/herobrine2nether/netherLauncher/gui/OutputMonitor.java
+++ src/com/herobrine2nether/netherLauncher/gui/OutputMonitor.java
@@ -1,12 +1,14 @@
 package com.herobrine2nether.netherLauncher.gui;
 
 import com.herobrine2nether.netherLauncher.osSpecific.FileSystem;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOError;
+import java.io.IOException;
 
 public class OutputMonitor {
-    static File assetsDir = new File(FileSystem.osSlash() + ".NetherLauncher");
+    static File assetsDir = new File("/.NetherLauncher");
     public static void Console() {
         if (!assetsDir.exists()) {
             initialSetup();
@@ -16,9 +18,9 @@ public class OutputMonitor {
     }
 
     public static void initialSetup() {
-        try{
-            assetsDir.mkdirs();
-        } catch (IOError e) {
+        try {
+            FileUtils.forceMkdir(assetsDir);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
