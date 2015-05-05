a260ef0113c7b2424d5ba3cdbdd9151edb921b29
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-05-05 12:26:05 -0600

    Adding Changelog Page to Source
    Icon Redo Next

--- src/com/herobrine2nether/netherLauncher/Main.java
+++ src/com/herobrine2nether/netherLauncher/Main.java
@@ -5,19 +5,6 @@ import com.herobrine2nether.netherLauncher.fileInterfaces.FileSetup;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            Thread.sleep(0);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-
-            public void run() {
-                FileSetup.Check();
-            }
-        });
-
+            FileSetup.Check();
     }
-
-
 }
